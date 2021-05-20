556a9ae7a607ab2be399c69e77a6d46788346e44
mercy021 <kinyanjuimercywangari@gmail.com>
2021-05-20 21:36:08 +0300

    first commit

---
+++ all.js
@@ -0,0 +1,56 @@
+//objects are collection of functions(object)
+//there are no classes in js instead we use object .
+//functions-its a block of code written to perform a specific task.
+//statements-group of words numbers,operators that perform specific task.
+// var a=b*5//this is an assignment expression.
+// console.log(a);
+let a='I wanna ';
+console.log(a+='bake a lemon cake')
+var jina='Hello Nancy'//illustration of how we use console.log command.
+console.log(jina);
+//  name =prompt('This is my fruit');
+// console.log(fruit);
+//values and types
+//adding a number to a string
+var x = "Mazda" + 16
+console.log(x);
+//boolean
+var x = 10;
+var b = 15;
+var c = 20;
+console.log(x == b);      
+console.log(x == c);
+//strings
+var guitar="I love playing acoustic guitar"
+console.log(guitar);
+//numbers-javascript has only one type of number,
+//they can be written,with all without decimals
+var lemon1=6
+var mint1=10.3+35
+console.log(lemon1);
+console.log(mint1);
+//arrays 
+//written with square brackets
+var fruits=["mango","pineapple","orange","water melon"]
+console.log(fruits);
+//objects
+//written using curly braces
+var musician={firstname:"Mercy",County:"Nairobi",Networth:"1 million",Age:"21"}
+console.log(musician);
+//undefined-variable without a value is undefined
+var bottle;
+console.log(bottle);
+//primitive data types are-booleans
+                          //-strings
+                          //undefined
+                          //numbers
+                                       `//null
+                          var person = {firstName:"banana cake", lastName:"fruit cake", price:50, Color:"green,red"};
+                          cakes=null;
+                          console.log(cakes);
+                          //empty values
+                          var pencil  = "";
+                          console.log(pencil);
+
+
+
